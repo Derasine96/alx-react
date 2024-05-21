7f47b938eae0fd2ef373d18a7615fecc70f4e361
Obikwelu Chidera <chidexobikwelu@gmail.com>
2024-05-21 14:52:18 +0100

    Introduction to react

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import holbertonlogo from './holberton-logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={holbertonlogo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" placeholder="Enter your email" /><br />
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" placeholder="Enter your password" /><br />
+        </form>
+        <button type="button">OK</button>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
