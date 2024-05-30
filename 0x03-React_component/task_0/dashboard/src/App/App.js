2e98145ab0724707e1114e09beaa6e9f87588017
Obikwelu Chidera <chidexobikwelu@gmail.com>
2024-05-30 11:02:11 +0100

    Fix tests

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,27 +1,41 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+
+import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
-import Notifications from '../Notifications/Notifications';
 import CourseList from '../CourseList/CourseList';
+import Notifications from '../Notifications/Notifications';
 import './App.css';
+import PropTypes from 'prop-types';
+import { getLatestNotification } from '../utils/utils';
 
-class App extends Component {
-  render() {
-    const { isLoggedIn } = this.props;
-
-    return (
-      <React.Fragment>
-        <Notifications />
-        <div className='App'>
-          <Header />
-          {isLoggedIn ? <CourseList /> : <Login />}
-          <Footer />
-        </div>
-      </React.Fragment>
-    );
-  }
+class App extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<div className='App'>
+					<div className='heading-section'>
+						<Notifications listNotifications={this.listNotifications} />
+						<Header />
+					</div>
+					{this.props.isLoggedIn ? (
+						<CourseList listCourses={this.listCourses} />
+					) : (
+						<Login />
+					)}
+					<Footer />
+				</div>
+			</React.Fragment>
+		);
+	}
 }
+
+App.defaultProps = {
+	isLoggedIn: false,
+};
+
+App.propTypes = {
+	isLoggedIn: PropTypes.bool,
+};
 
 export default App;
